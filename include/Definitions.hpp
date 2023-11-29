c74cd20f84330fa6c283510521fe57d6a62af055
RyosukeKusumoto <kusuprograming@gmail.com>
2023-11-29 12:57:35 +0900

    added glm as dependencies

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -1,5 +1,5 @@
-#define ASSET_PATH "C:/Users/ienvs/source/repos/opencv_ar_object/assets/"
-#define SHADER_PATH "C:/Users/ienvs/source/repos/opencv_ar_object/shaders/"
-#define BUILD_PATH "C:/Users/ienvs/source/repos/opencv_ar_object/build"
+#define ASSET_PATH "/home/ryosuke/opencv_ar_object/assets/"
+#define SHADER_PATH "/home/ryosuke/opencv_ar_object/shaders/"
+#define BUILD_PATH "/home/ryosuke/opencv_ar_object/build"
 #define PROJECT_BUILD_TYPE "Debug"
 /* #undef BUILD_TYPE */
